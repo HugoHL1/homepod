b312635406b90bc5e2a58109d9508a759a948cd1
IlièsBenos <iliesbenosman@gmail.com>
2019-02-20 16:43:09 +0100

    Merge branch 'master' of https://github.com/HugoLeroyHL/homepod
    branch

--- scripts/techspecscript.js
+++ scripts/techspecscript.js
@@ -1,1 +1,28 @@
-const panel = document.querySelector("#panelRight")
+
+function Scroll(){
+let ypos = window.pageYOffset
+let leftBoxtitleOne = document.querySelector("#techspec_boxtitle_animl")
+let leftBoxparaOne = document.querySelector("#techspec_desc_animl")
+let leftBoxtitleTwo = document.querySelector("#techspec_boxtitle_animr")
+let leftBoxparaTwo = document.querySelector("#techspec_desc_animr")
+  if(ypos> 0){
+    leftBoxtitleOne.style.opacity = "0"
+    leftBoxparaOne.style.opacity = "0"
+    leftBoxtitleTwo.style.opacity = "0"
+    leftBoxparaTwo.style.opacity = "0"
+}
+  if (ypos > 200){
+    leftBoxtitleOne.style.opacity = "1"
+    leftBoxparaOne.style.opacity = "1"
+    leftBoxtitleOne.classList.add("techspec_boxtitle_effectl")
+    leftBoxparaOne.classList.add("techspec_desc_effectl")
+  }
+  if(ypos > 800){
+    leftBoxtitleTwo.style.opacity = "1"
+    leftBoxparaTwo.style.opacity = "1"
+    leftBoxtitleTwo.classList.add('techspec_boxtitle_effectr')
+    leftBoxparaTwo.classList.add('techspec_desc_effectr')
+   }
+
+}
+window.addEventListener("scroll",Scroll)
