600818e462698d7a4489a624082dcad20beb43c4
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-02-20 15:32:34 +0100

    Push Techspech pour js
    js (pas facile)

--- scripts/techspecscript.js
+++ scripts/techspecscript.js
@@ -1,1 +1,24 @@
-const panel = document.querySelector("#panelRight")
+function Scroll(){
+let leftBoxtitleOne = window.querySelector("techspec_desc_animl")
+let leftBoxparaOne = window.querySelector("techspec_desc_animl")
+let leftBoxtitleTwo = window.querySelector("techspec_boxtitle_animr")
+let leftBoxparaTwo = window.querySelector("techspec_desc_animr")
+let ypos = window.pageYOffse
+  if(ypos > 300){
+     leftBoxtitleOne.classList.add("techspec_boxtitle_effectl")
+     leftBoxparaOne.classList.add("techspec_desc_effectl")
+  }
+  // if(ypos < 500){
+  //
+  // }
+  // if(ypos>800){
+  //
+  // }
+  // if(ypos<1050){
+  //
+  // }
+  // if(ypos>460){
+  //
+  // }
+}
+window.addEventListener("scroll",Scroll)
