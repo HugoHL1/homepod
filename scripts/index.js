3490c2a6ccff359e7cfbd3ec3d0e58e6feaf4f50
IlièsBenos <iliesbenosman@gmail.com>
2019-02-21 19:38:53 +0100

    zz
    zz

--- scripts/index.js
+++ scripts/index.js
@@ -23,65 +23,8 @@ let index_imageHomePod = document.querySelector('.index_imageHomePod')
     tAnimation.classList.add("index_title_animation2")
     pAnimation.classList.add("index_p_animation2")
   }
-  // if(ypos> 320){
-  //   // imgHomePod.classList.add("imgHomePod3")
-  //   // pAnimation.classList.remove("index_p_animation")
-  //
-  // }
-  if(ypos > 470){
-    imgHomePod.classList.add("imgHomePod")
-    // imgHomePod.classList.add("imgHomePod5")
-    // imgHomePod.style.transitionTimingFunction= "ease"
-  }
-
-  // if ((ypos > 400) && (ypos > 0 )) {
-  //   imgHomePod.classList.add("imgHomePod4")
-  // }
-  // if ( ypos > 10)
-    // if(ypos<500){
-    //   imgHomePod.classList.remove("imgHomePod4")
-    //
-    // }
-
-    // if(ypos < 330){
-    //   imgHomePod.classList.remove("imgHomePod2")
-    //   imgHomePod.classList.remove("imgHomePod3")
-    //   // imgHomePod.classList.add("imgHomePod4")
-    //   // pAnimation.classList.add("index_p_animation")
-    //   pAnimation.classList.remove("index_p_animation2")
-    // }
-    // if (ypos<310) {
-    //   imgHomePod.classList.add("imgHomePod4")
-    //
-    // }
-    // if (ypos < 320){
-    //   imgHomePod.classList.remove("imgHomePod4")
-    // }
-  // if(ypos<500){
-  //   imgHomePod.classList.remove("imgHomePod3")
-  // }
-
-
-  // if(ypos<450){
-  // }
-  // if(ypos>460){
-  //   // index_container_animation.classList.add("eza2")
-  //   // imgHomePod.style.position = "sticky"
-  //   // pAnimation.classList.remove("index_p_animation2")
-  //   imgHomePod.style.position = "sticky"
-  //   imgHomePod.style.top = "-70px"
-  //   // pAnimation.classList.add("index_p_animation")
-  // }
-  // if (ypos>650){
-  // }
-   // if(ypos>500){
-  //   pAnimation.classList.add("aos-animate")
-  // }
 }
 window.addEventListener("scroll",Scroll);
-
-
-
 
 
 // Animation Carrousel
