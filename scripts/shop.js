b3637200074f7a0c789c1d8bddfba096f1f89f91
Eythan <eythan.saillet@gmail.com>
2019-02-21 14:21:49 +0100

    Responsive header

--- scripts/shop.js
+++ scripts/shop.js
@@ -2,7 +2,6 @@ let menuMobile = document.querySelector('.header_nav')
 let buttonHamburger = document.querySelector('.header_divhamburger img')
 
 
-
 buttonHamburger.addEventListener('click',function(){
   if (menuMobile.classList.contains("header_nav_visible")) {
     menuMobile.classList.remove("header_nav_visible")
